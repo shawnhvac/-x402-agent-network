d9a8412d66a1ad3b462f064da68150e7a2180371
Shawn Hvac <shawnhvac@example.com>
2026-04-11 07:46:07 +0000

    Fix: Add buildscript block for Gradle dependencies

--- x402-agent-network/build.gradle.kts
+++ x402-agent-network/build.gradle.kts
@@ -1,4 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
+
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
@@ -9,6 +20,5 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
